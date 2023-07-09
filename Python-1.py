9e458e58cc6a9810057b187d6b5a0b4ab3cac38c
mhmd-yhy <138642434+mhmd-yhy@users.noreply.github.com>
2023-07-09 17:37:44 +0300

    Update Python-1.py

--- Python-1.py
+++ Python-1.py
@@ -1,11 +1,11 @@
 def solution(inp):
-    buyuk = []
+    sonuc = []
     for x in range(len(inp)-1):
         carpma = inp[x]*inp[x+1]
-        buyuk.append(carpma)
+        sonuc.append(carpma)
         print(carpma)
     print("en büyük çarpma :")
-    return max(buyuk)
+    return max(sonuc)
 
 inp = [5,6,9,2,7,5]
 print(solution(inp))
