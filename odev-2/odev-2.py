79617f21cf17a6d8820d9ee215e13fda9685952b
mhmd-yhy <138642434+mhmd-yhy@users.noreply.github.com>
2023-07-14 10:00:09 +0300

    Update odev-2.py

--- odev-2/odev-2.py
+++ odev-2/odev-2.py
@@ -1,15 +1,21 @@
 #SORU-1
+#SORU-1
 def solution(lst):
     # Listeyi sıralamak
     b = sorted(lst)
-    print(b)
+    num = []
+    print(f"b = {b}")
     for x in b:
-        if not b.__contains__(x + 1):
-            return (x + 1)
+         if not b.__contains__(x + 1) and x+1 < 0:
+             num.append((x + 1)*-1)
+         elif not b.__contains__(x + 1) and x+1 > 0:
+             num.append(x+1)
+    print(f"num = {num}")
+    return min(num)
 
-#a = [0,-1,-2,-3,-4]
+#a = [0,1,-7,-1,-2,-3,-4]
 #a = [1,2,3,5,7,8,9]
-a = [1,7,10,5,-1,8,9,0]
+a = [1,2,10,5,-7,-1,8,9,0]
 birinci_soru_cevab = solution(a)
 
 """____________________________________________________________________________________________"""
